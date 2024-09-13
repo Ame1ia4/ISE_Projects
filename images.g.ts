18dfd9a69405106ae7d539dae36f27ddc2c3ac5d
Ame1ia4 <151676710+Ame1ia4@users.noreply.github.com>
2024-09-13 08:23:42 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "Turtle":return hex`0078000408020103001c0001dc00690000045e010004000000000000000000000500000100000314000000040002162220002400012930003400021124`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
